1f725bb7b000aca0d71fcd73ef965e09ea924fd4
Ren <renardchien@gmail.com>
2014-10-29 14:52:49 -0400

    Updated to pbkdf2 and starting to use redis

---
+++ src/controllers/Domo.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+var models = require('../models');
+
+var Domo = models.Domo;
+
+var makerPage = function(req, res) {
+
+};
+
+var makeDomo = function(req, res) {
+
+  
+};
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
---
+++ src/models/Domo.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+var DomoModel;
+
+var setName = function(name) {
+    return _.escape(name).trim();
+};
+
+var DomoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        set: setName
+    },
+    
+    age: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    
+    owner: 	{
+		type: mongoose.Schema.ObjectId,
+		required: true,
+		ref: 'Account'
+	},
+    
+    createdData: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+DomoSchema.methods.toAPI = function() {
+    return {
+        name: this.name,
+        age: this.age
+    };
+};
+
+DomoSchema.statics.findByOwner = function(ownerId, callback) {
+
+
+};
+
+
+DomoModel = mongoose.model('Domo', DomoSchema);
+
+
+module.exports.DomoModel = DomoModel;
+module.exports.DomoSchema = DomoSchema;
