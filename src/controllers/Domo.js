072b1c8ca1f5bbc1bfb91af1b8893ead76a74012
Dustin Raimondi <raimondi1337@gmail.com>
2014-11-16 15:02:25 -0500

    playing with jade and stuff, probably broke everything

--- src/controllers/Domo.js
+++ src/controllers/Domo.js
@@ -1,36 +1,36 @@
 var _ = require('underscore');
 var models = require('../models');
 
-var Domo = models.Domo;
+var Scrape = models.Scrape;
 
 var makerPage = function(req, res) {
     
-    Domo.DomoModel.findByOwner(req.session.account._id, function(err, docs) {
+    Scrape.ScrapeModel.findByOwner(req.session.account._id, function(err, docs) {
 
         if(err) {
             console.log(err);
             return res.status(400).json({error:'An error occurred'}); 
         }
         
-        res.render('app', {domos: docs});
+        res.render('app', {scrapes: docs});
     });
 };
 
-var makeDomo = function(req, res) {
+var addScrape = function(req, res) {
 
     if(!req.body.name || !req.body.age) {
-        return res.status(400).json({error: "RAWR! Both name and age are required"});
+        return res.status(400).json({error: "Both URL and Query are required"});
     }
     
-    var domoData = {
-        name: req.body.name,
-        age: req.body.age,
+    var scrapeData = {
+        url: req.body.url,
+        query: req.body.query,
         owner: req.session.account._id
     };
     
-    var newDomo = new Domo.DomoModel(domoData);
+    var newScrape = new Scrape.ScrapeModel(scrapeData);
     
-    newDomo.save(function(err) {
+    newScrape.save(function(err) {
         if(err) {
             console.log(err);
             return res.status(400).json({error:'An error occurred'}); 
@@ -42,4 +42,4 @@ var makeDomo = function(req, res) {
 };
 
 module.exports.makerPage = makerPage;
-module.exports.make = makeDomo;
+module.exports.make = addScrape;
