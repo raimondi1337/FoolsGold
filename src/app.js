76bbad7495fd1aebf347d9fc0b63c0744581591a
Dustin Raimondi <raimondi1337@gmail.com>
2014-11-16 14:11:37 -0500

    switched to LoginApp codebase

--- src/app.js
+++ src/app.js
@@ -1,28 +1,4 @@
 //import libraries 
-<<<<<<< HEAD
-var path = require('path'); //path handles file system paths
-var express = require('express'); //Express is an MVC server framework
-var compression = require('compression'); //compression is used to gzip packets into smaller/faster sizes
-var favicon = require('serve-favicon'); //serve-favicon serves the favicon quickly without needing to code a controller or route
-var cookieParser = require('cookie-parser'); //cookie parser pulls cookie data from HTTP packets
-//pull in our routes file to handle all of the URL routes
-var router = require('./router.js'); 
-
-var server;  //the http server
-var port = process.env.PORT || process.env.NODE_PORT || 3000; //server port based on environment falling back to 3000 if needed
-
-var app = express(); //the express MVC app
-app.use('/assets', express.static(path.resolve(__dirname+'../../client/'))); //sets the static file folder (css, images, etc) to the url /assets
-app.use(compression()); //tell express to use compression for smaller/faster packets
-app.set('view engine', 'jade'); //set the view type to Jade
-app.set('views', __dirname + '/views'); //set the views folder (for use with the controllers)
-app.use(favicon(__dirname + '/../client/img/favicon.png')); //set the favicon
-app.use(cookieParser()); //tell express to use the cookie parser
-
-router(app); //pass the app to our router module to set up the URL routes
-
-//tell the express app to listen on the given port and get the HTTP server back
-=======
 var path = require('path'); 
 var express = require('express');  
 var compression = require('compression');  
@@ -84,7 +60,6 @@ app.use(cookieParser());
 
 router(app); 
 
->>>>>>> parent of f64b764... removed jade
 server = app.listen(port, function(err) { 
     if (err) {
       throw err;
--- src/router.js
+++ src/router.js
@@ -1,19 +1,5 @@
 //import the controller folder (automatically calls the index.js file)
 var controllers = require('./controllers'); 
-<<<<<<< HEAD
-
-//router function to set up the URL routes for an Express MVC app
-var router = function(app) {
-
-    app.get('/', controllers.main); //map the main site page to controllers.main
-    app.get('/variables', controllers.getVariablePage); //map the /variables page to controllers.getVariablePage
-    app.get('/extended', controllers.getExtended); //map the /extended page to controllers.getExtended
-    app.get('/gallery', controllers.getGallery); //map the /gallery page to controllers.getGallery
-    app.post('/time', controllers.updateTime); //map the /time page to controllers.updateTime
-};
-
-module.exports = router; //export the router as a public function
-=======
 var mid = require('./middleware');
 
 var router = function(app) {
@@ -29,4 +15,3 @@ var router = function(app) {
 };
 
 module.exports = router; 
->>>>>>> parent of f64b764... removed jade
