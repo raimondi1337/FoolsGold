2d64bd50cc887a29ec1bd8e574579050ab99ea45
Dustin Raimondi <raimondi1337@gmail.com>
2014-11-16 17:15:54 -0500

    blah

---
+++ src/controllers/Scrape.js
@@ -0,0 +1,45 @@
+var _ = require('underscore');
+var models = require('../models');
+
+var Scrape = models.Scrape;
+
+var makerPage = function(req, res) {
+    
+    Scrape.ScrapeModel.findByOwner(req.session.account._id, function(err, docs) {
+
+        if(err) {
+            console.log(err);
+            return res.status(400).json({error:'An error occurred'}); 
+        }
+        
+        res.render('app', {scrapes: docs});
+    });
+};
+
+var makeScrape = function(req, res) {
+
+    if(!req.body.url || !req.body.query) {
+        return res.status(400).json({error: "Both URL and Query are required"});
+    }
+    
+    var scrapeData = {
+        url: req.body.url,
+        query: req.body.query,
+        owner: req.session.account._id
+    };
+    
+    var newScrape = new Scrape.ScrapeModel(scrapeData);
+    
+    newScraoe.save(function(err) {
+        if(err) {
+            console.log(err);
+            return res.status(400).json({error:'An error occurred'}); 
+        }
+
+        res.json({redirect: '/maker'});
+    });
+    
+};
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeScrape;
---
+++ src/models/Scrape.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+var ScrapeModel;
+
+var setURL = function(url) {
+    return _.escape(url).trim();
+};
+
+var ScrapeSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true,
+        trim: true,
+        set: setURL
+    },
+    
+    query: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    
+    owner: 	{
+		type: mongoose.Schema.ObjectId,
+		required: true,
+		ref: 'Account'
+	},
+    
+    createdData: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+ScrapeSchema.methods.toAPI = function() {
+    return {
+        url: this.url,
+        query: this.query
+    };
+};
+
+ScrapeSchema.statics.findByOwner = function(ownerId, callback) {
+
+    var search = {
+        owner: mongoose.Types.ObjectId(ownerId)
+    };
+
+    return ScrapeModel.find(search).select("url query").exec(callback);
+};
+
+
+ScrapeModel = mongoose.model('Scrape', ScrapeSchema);
+
+
+module.exports.ScrapeModel = ScrapeModel;
+module.exports.ScrapeSchema = ScrapeSchema;
