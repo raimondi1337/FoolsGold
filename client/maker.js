266bf5dcdc30c05ebf98dcabb4d6fd67f019a865
Dustin Raimondi <raimondi1337@gmail.com>
2014-11-16 17:42:42 -0500

    blah

--- client/maker.js
+++ client/maker.js
@@ -27,17 +27,17 @@ $(document).ready(function() {
         });        
     }
     
-    $("#makeDomoSubmit").on("click", function(e) {
+    $("#makeScrapeSubmit").on("click", function(e) {
         e.preventDefault();
     
         $("#domoMessage").animate({width:'hide'},350);
     
-        if($("#domoName").val() == '' || $("#domoAge").val() == '') {
-            handleError("RAWR! All fields are required");
+        if($("#scrapeURL").val() == '' || $("#scrapeQuery").val() == '') {
+            handleError("All fields are required");
             return false;
         }
 
-        sendAjax($("#domoForm").attr("action"), $("#domoForm").serialize());
+        sendAjax($("#scrapeForm").attr("action"), $("#scrapeForm").serialize());
         
         return false;
     });
