b30bef615baa3b12270697dcd7d55f4fa14f1c07
Dustin Raimondi <raimondi1337@gmail.com>
2014-11-16 19:37:09 -0500

    blah

--- src/router.js
+++ src/router.js
@@ -9,8 +9,8 @@ var router = function(app) {
     app.get("/signup", mid.requiresSecure,mid.requiresLogout, controllers.Account.signupPage);
     app.post("/signup", mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
     app.get("/logout", mid.requiresLogin, controllers.Account.logout);
-    //app.get("/maker", mid.requiresLogin, controllers.Scrape.makerPage);
-    //app.post("/maker", mid.requiresLogin, controllers.Scrape.make);
+    app.get("/maker", mid.requiresLogin, controllers.Scrape.makerPage);
+    app.post("/maker", mid.requiresLogin, controllers.Scrape.make);
     app.get("/", mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
