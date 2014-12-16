e53d0ec8d63c684dfbcfd60a0742e4b64f6df8bd
Dustin Raimondi <raimondi1337@gmail.com>
2014-12-15 19:58:07 -0500

    added request and cheerio, wrote a hard coded test query to get all of the links on a page

--- src/controllers/ScrapeGallery.js
+++ src/controllers/ScrapeGallery.js
@@ -19,24 +19,25 @@ var makerPage = function(req, res) {
     });
 };
 
+//initial scrape
+function testRequest(){
+    var testURL = 'http://forum.miata.net/vb/forumdisplay.php?f=124';
+    request(testURL, function(error, response, html){
+        if(!error){
+            return($.makeArray($('a')));
+        }
+    });
+}
+
 var makeScrape = function(req, res) {
     if(!req.body.url || !req.body.query) {
         return res.status(400).json({error: "Both URL and Query are required"});
     }
-
-    //initial scrape
-    var testURL = 'http://forum.miata.net/vb/forumdisplay.php?f=124';
-    var testResults;
-    request(testURL, function(error, response, html){
-        if(!error){
-            testResults = $('a').html();
-        }
-    });
     
     var scrapeData = {
         url: req.body.url,
         query: req.body.query,
-        results: testResults,
+        results: testRequest,
         owner: req.session.account._id
     };
     
