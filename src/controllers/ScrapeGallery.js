264538817b7cad2ffef0726354e37f32ad8c4272
Dustin Raimondi <raimondi1337@gmail.com>
2014-12-15 19:47:11 -0500

    added request and cheerio, wrote a hard coded test query to get all of the links on a page

--- src/controllers/ScrapeGallery.js
+++ src/controllers/ScrapeGallery.js
@@ -1,4 +1,7 @@
 var _ = require('underscore');
+var request = require('request');
+var cheerio = require('cheerio');
+
 var models = require('../models');
 
 var Scrape = models.Scrape;
@@ -20,11 +23,20 @@ var makeScrape = function(req, res) {
     if(!req.body.url || !req.body.query) {
         return res.status(400).json({error: "Both URL and Query are required"});
     }
+
+    //initial scrape
+    var testURL = 'http://forum.miata.net/vb/forumdisplay.php?f=124';
+    var testResults;
+    request(testURL, function(error, response, html){
+        if(!error){
+            testResults = $('a');
+        }
+    });
     
     var scrapeData = {
         url: req.body.url,
         query: req.body.query,
-        results: [{url: 'http://supermotojunkie.com/forumdisplay.php?72-Bikes'},{url: 'http://forum.miata.net/vb/forumdisplay.php?f=99'}],
+        results: testResults,
         owner: req.session.account._id
     };
     
