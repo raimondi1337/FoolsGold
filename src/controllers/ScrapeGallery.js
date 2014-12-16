7721157abb638ec37d4b7ca07f5d6f205618107b
Dustin Raimondi <raimondi1337@gmail.com>
2014-12-16 17:44:25 -0500

    added request and cheerio, wrote a hard coded test query to get all of the links on a page

--- src/controllers/ScrapeGallery.js
+++ src/controllers/ScrapeGallery.js
@@ -21,20 +21,22 @@ var makerPage = function(req, res) {
 
 function doScrape(link, query){
     var x = [];
-    console.log('x before='+x);
+    console.log('link = '+link);
     request(link, function (error, response, html) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(html);
+            console.log('query = '+query);
             var tRes= $("a:contains(query)").each(function(){
                 var t = $(this).text();
+                console.log('t = '+t);
                 var a = $(this).attr('href');
+                console.log('a = '+a);
                 var item={url: a, text: t};
                 console.log('item found - '+item);
                 x.push(item);
             });
         }
     });
-    console.log('x after='+x);
     return x;
 }
 
@@ -44,8 +46,7 @@ var makeScrape = function(req, res) {
     }
 
     var currentResults = doScrape(req.body.url, req.body.query);
-    console.log('zxcvbnm');
-    console.log(currentResults);
+    console.log('results found: '+currentResults);
     
     var scrapeData = {
         url: req.body.url,
