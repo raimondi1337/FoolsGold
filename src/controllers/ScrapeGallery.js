17eaacb35ded26d22d1ae87baab7c572cf4462f3
Dustin Raimondi <raimondi1337@gmail.com>
2014-12-17 12:39:50 -0500

    added request and cheerio, wrote a hard coded test query to get all of the links on a page

--- src/controllers/ScrapeGallery.js
+++ src/controllers/ScrapeGallery.js
@@ -20,13 +20,14 @@ var makerPage = function(req, res) {
 };
 
 function doScrape(link, query, callback){
+    console.log("query = "+query);
     var x = [];
     //download the page at the url link
     request(link, function (error, response, html) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(html);
             //get all of the links on the page that contain the query
-            $("a:contains(query)").each(function(){
+            $("a:contains('"+query+"')").each(function(){
                 var t = $(this).text();
                 var a = $(this).attr('href');
                 //itemize the link and the text associated with it
